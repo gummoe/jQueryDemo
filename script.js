1a9faa9dc8a4265b6edb60dc5480d5ec48b83983
Benjamin Gummoe <Gummoe@BENJAMINs-Air-2.home>
2015-01-07 22:52:36 -0500

    Added slide and explode buttons, reoriented UI for horizontal controls

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ $(document).ready(function() {
 	$(function () {
   		$('[data-toggle="popover"]').popover()
 	});
+	
+	$('.slide').click(function() {
+		$('#the-div').effect('slide');
+	});
+
+	$('.explode').click(function() {
+		$('#the-div').effect('explode');
+	});
 	// $('#the-div').click(function() {
 	// 	// $(this).animate({left: '+=50px'}, 500);
 		
